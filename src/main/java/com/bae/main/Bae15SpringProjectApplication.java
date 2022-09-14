7bc78532f19f8aa448bb43ab2cf74cf7c95104b3
MII2A <mariam.bela@yahoo.com>
2022-09-14 02:41:53 +0100

    MS: git init

---
+++ src/main/java/com/bae/main/Bae15SpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bae.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bae15SpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bae15SpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bae/main/Bae15SpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bae.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bae15SpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
